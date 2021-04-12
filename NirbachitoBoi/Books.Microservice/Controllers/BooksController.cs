b1d867be5b71a654c4014b9189da009e8e9cf7de
Alimur Razi Rana <alimur.razi@selise.ch>
2021-04-12 09:51:03 +0600

    feat: ocelot api add

---
+++ NirbachitoBoi/Books.Microservice/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static readonly string[] Books = new[]
+{
+            "Feluda somogro", "Hasuli baker upokotha"
+        };
+
+        [HttpGet]
+        public string[] Get()
+        {
+            return Books;
+        }
+    }
+}
