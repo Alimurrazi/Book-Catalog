f3406d03b0bb3f837111abe860fcca2bcd8149cf
Alimur Razi Rana <alimurrazi570@gmail.com>
2024-05-26 15:39:29 +0600

    feat: .net 6 upgraded

--- src/BookStore.API/Program.cs
+++ src/BookStore.API/Program.cs
@@ -2,10 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.API.Configuration;
+using BookStore.Application.Configuration;
+using BookStore.Domain.Interfaces;
+using BookStore.Infrastructure.Context;
+using BookStore.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BookStore.API
 {
@@ -13,14 +23,57 @@ namespace BookStore.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            var configuration = builder.Configuration;
+
+            builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("BookDbConnection")));
+
+            builder.Services.AddAutoMapper(typeof(AutomapperConfig));
+            builder.Services.AddControllers();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
                 {
-                    webBuilder.UseStartup<Startup>();
+                    Title = "BookStore API",
+                    Version = "v1"
                 });
+            });
+
+            builder.Services.AddCors();
+
+            builder.Services.ResolveDependencies();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.Run();
+        }
     }
 }
